5156c8d8da2febb7352d6f5f43f5ab1cd2c95140
KingTechnician <ikfreeman@yahoo.com>
2023-11-14 14:44:50 -0500

    Initial commit

---
+++ ApplePieMobile/Game.swift
@@ -0,0 +1,36 @@
+//
+//  Game.swift
+//  ApplePieMobile
+//
+//  Created by Isaiah Freeman  on 11/14/23.
+//
+
+import Foundation
+
+struct Game{
+    var word:String
+    var incorrectMovesRemaining:Int
+    var guessedLetters: [Character]
+    var formattedWord:String{
+        var guessedWord = ""
+        for letter in word{
+            if guessedLetters.contains(letter){
+                guessedWord+="\(letter)"
+            }
+            else
+            {
+                guessedWord+="_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessed(letter:Character)
+    {
+        guessedLetters.append(letter)
+        if !word.contains(letter)
+        {
+            incorrectMovesRemaining-=1
+        }
+    }
+}
